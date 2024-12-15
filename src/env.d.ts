27b5a1569504bb41aeccc1839224c392d742080a
Diego <diego.lopez.mardomingo@gmail.com>
2024-12-16 00:06:12 +0100

    Dynamic redirect_url for dif environment

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string
   readonly SUPABASE_KEY: string
+  readonly REDIRECT_URL: string
 }
 
 interface ImportMeta {
--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -11,7 +11,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: "http://localhost:4321/api/auth/callback"
+        redirectTo: import.meta.env.REDIRECT_URL + "api/auth/callback"
       },
     });
 
