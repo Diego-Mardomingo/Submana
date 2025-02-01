eacddf6947553f19db1015f0f9e5ba57281e2733
Diego <diego.lopez.mardomingo@gmail.com>
2025-02-02 00:59:14 +0100

    Style changes and added options btns + monthly spent

--- src/components/CalendarBody.jsx
+++ src/components/CalendarBody.jsx
@@ -9,10 +9,10 @@ export default function CalendarBody({ initialYear, initialMonth }) {
   const [month, setMonth] = useState(initialMonth);
 
   // Arrays estáticos
-  const diasSemana = ["LUN","MAR","MIE","JUE","VIE","SAB","DOM"];
+  const diasSemana = ["MON","TUE","WED","THU","FRY","SAT","SUN"];
   const meses = [
-    "Enero","Febrero","Marzo","Abril","Mayo","Junio",
-    "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
+    "January","February","March","April","May","June",
+    "July","August","September","October","November","December"
   ];
 
   // Función que nos da el número de días en un mes dado
@@ -38,7 +38,6 @@ export default function CalendarBody({ initialYear, initialMonth }) {
   const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
 
   // Para posicionar el primer día en la rejilla
-  // new Date(year, month, 1).getDay() => 0 = domingo, 1 = lunes, etc.
   const firstDayOfMonth = new Date(year, month, 1).getDay();
   // Ajuste: si getDay() es 0 (domingo), queremos que sea la columna 7
   const startColumn = firstDayOfMonth === 0 ? 7 : firstDayOfMonth;
@@ -73,25 +72,66 @@ export default function CalendarBody({ initialYear, initialMonth }) {
     setYear(currentYear);
   }
 
+  function todayIcon(){
+    return (
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-home"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+    );
+  }
+  function anteriorIcon(){
+    return (
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg> 
+    );
+  }
+  function siguienteIcon(){
+    return(
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+    );
+  }
+  function newIcon(){
+    return(
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" /><path d="M15 12h-6" /><path d="M12 9v6" /></svg>
+    );
+  }
+  function dashboardIcon(){
+    return (
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-presentation-analytics"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 12v-4" /><path d="M15 12v-2" /><path d="M12 12v-1" /><path d="M3 4h18" /><path d="M4 4v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-10" /><path d="M12 16v4" /><path d="M9 20h6" /></svg>
+    );
+  }
+  function mySubsIcon(){
+    return (
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-menu-3"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 6h10" /><path d="M4 12h16" /><path d="M7 12h13" /><path d="M4 18h10" /></svg>
+    );
+  }
+
   return (
     <div className="calendar_container">
-      {/* Header con botones y nombre del mes/año */}
+      <nav className="options_btn">
+        <ul>
+          <li className="mySubs_btn">{mySubsIcon()}</li>
+          {/* <li className="dashboard_btn" title="My Dashboard">{dashboardIcon()}</li> */}
+          <li className="new_btn" title="New sub">{newIcon()}</li>
+        </ul>
+      </nav>
       <header className="calendar_header">
         <div className="buttonsMonth">
           <button onClick={handlePrevMonth} className="anterior">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>
+            {anteriorIcon()}
           </button>
           <button onClick={handleNextMonth} className="siguiente">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>
+            {siguienteIcon()}
           </button>
           <button onClick={handleToday}>
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-home"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+            {todayIcon()}
           </button>
         </div>
         <div className="header_text">
           <p className="nombre_mes">{meses[month]}</p>
           <p className="año">{year}</p>
         </div>
+          <div className="spent_container">
+            <p className="spent_title">Monthly spent</p>
+            <p className="spent_value">298.43€</p>
+          </div>
       </header>
 
       {/* Sección del calendario */}
