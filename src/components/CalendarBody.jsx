f56baeeeedbb37a165db721efc38404af53bdd0c
Diego <diego.lopez.mardomingo@gmail.com>
2025-02-02 17:17:05 +0100

    New svg for adding subs and new redirection

--- src/components/CalendarBody.jsx
+++ src/components/CalendarBody.jsx
@@ -89,7 +89,7 @@ export default function CalendarBody({ initialYear, initialMonth }) {
   }
   function newIcon(){
     return(
-      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" /><path d="M15 12h-6" /><path d="M12 9v6" /></svg>
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
     );
   }
   function dashboardIcon(){
@@ -109,7 +109,7 @@ export default function CalendarBody({ initialYear, initialMonth }) {
         <ul>
           <li className="mySubs_btn">{mySubsIcon()}</li>
           {/* <li className="dashboard_btn" title="My Dashboard">{dashboardIcon()}</li> */}
-          <li className="new_btn" title="New sub">{newIcon()}</li>
+          <li className="new_btn" onClick={ () =>{window.location.href = '/NewSub';}} title="New sub">{newIcon()}</li>
         </ul>
       </nav>
       <header className="calendar_header">
